24358c5841413a6e885e7d583ce0da19beb1aa91
rubo77 <github@r.z11.de>
2019-02-25 16:08:05 +0100

    add some more automation to transpile shell and add an example header for gluon shell scripts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Bash2Lua-transpiler
-Transpile Bash scripts into Lua sourcecode
+
+Transpile Bash and Shell scripts into Lua sourcecode.
+
+This is mainly used to translate scripts in packages that are developed for gluon
+but you can easily adapt it to your code style.
+
+The script is far from perfect! It should just take away some initial global
+regular expression replacement to start converting your script. A lot of work
+still has to follow to make the Lua script run.
--- bash2lua.sh
+++ bash2lua.sh
@@ -3,10 +3,74 @@
 # this scripts needs a bash file as input argument
 # that file will be converted into Lua source
 
+# first write out some defautl Lua header:
+# here a useful example to convert gluon shell scripts:
+cat << EOF
+#!/usr/bin/lua
+
+local uci = require("simple-uci").cursor()
+local util = require 'gluon.util'
+
+-- return_value is optional to return the exit code of the shell command instead of the output
+function shell_exec(command, return_value)
+  if return_value then
+    return os.execute(command .. ' &> /dev/null')
+  end
+  -- maybe use util.exec() instead, which does not use assert()
+  local file = assert(io.popen(command, 'r'))
+  local output = file:read('*all')
+  file:close()
+  return output
+end
+
+function logger(m)
+  shell_exec('logger -s -t "gluon-offline-ssid" -p 5 "' .. m .. '"')
+end
+EOF
+
 sed 's/fi$/end/;' $1 |\
 sed 's/if \[/if/g' |\
 sed 's/\]; then/then/g' |\
 sed 's/-eq/==/g' |\
+sed 's/-lt/</g' |\
+sed 's/-gt/>/g' |\
+sed 's/-ge/>=/g' |\
+sed 's/ \] || \[ / or /g' |\
 sed 's/if \(.*\) = /if \1 == /g' |\
-sed 's/\(\s*\)#\s*/\1-- /g' |\
+sed -E 's/\$\{#([^\}]*?)\}/String.length(\1)/g' |\
+sed -E 's/(\s*)#+\s*/\1-- /g' |\
+sed -E 's/echo ([^"]+)/print("\1")/g' |\
+sed -E 's/echo (.+)$/print(\1)/g' |\
+sed -E 's/"\$\(\((.*?)\)\)"/\1/g' |\
+sed -E 's/\$\(\((.*?)\)\)/\1/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/"([^\$]*)\$(\w+)([^"]*)"/"\1" .. \2 .. "\3"/g' |\
+sed -E 's/ .. ""//g' |\
+sed -E 's/"" .. //g' |\
+sed -E 's/\$(\w+)/\1/g' |\
+sed -E 's@-- !/bin/.*$@@g' |\
 cat
+# fi
+# if
+# then
+# == 
+# < 
+# > 
+# >= 
+# if =
+# comments
+# echo->print
+# $(())
+# variables autside "" 
+# repeat
+# repeat
+# repeat
+# repeat
+# repeat
+# variables inside "" 
